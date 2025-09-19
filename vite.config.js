b542cff25132f208a3277aeaea0c992b6ec60187
unknown <cse.jeoni@gmail.com>
2025-09-19 17:58:54 +0900

    빌드 시도5

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'app',
   build: {
-    outDir: '../dist/app',
+    rollupOptions: {
+      input: 'app/index.html',
+    },
+    outDir: 'dist/app',
     emptyOutDir: true,
   },
 })
